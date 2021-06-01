4b9405d13497447adc22e0bd753cc94731f98deb
mini <jongmin4422@naver.com>
2021-06-01 18:49:41 +0900

    change dataset

--- Rcode/보간법.R
+++ Rcode/보간법.R
@@ -3,7 +3,7 @@ library(xts)
 library(lubridate)
 
 setwd('C:/Users/jongm/OneDrive - 광운대학교/ProjecTS/TEAM-ID/dataset')
-ff <- read.csv(file='./hinix_short3.csv', header = TRUE)
+ff <- read.csv(file='./hynix_short2.csv', header = TRUE)
 d <- ts(ff) #타임 시리즈로 변환
 d
 sum(is.na(d)) #보간법 적용 전에 결측치 확인
@@ -16,12 +16,12 @@ approxValues <- na.approx(zooValues)
 plot(approxValues)
 sum(is.na(approxValues))#결측치 없어졌는지 확인
 
-write.csv(approxValues, file = './hinix_short4.csv')
+write.csv(approxValues, file = './hynix_short.csv')
 
 plot(approxValues$유가종가) 
 
 #후행보간법
 dd <- na.locf(zooValues, fromLast = TRUE) # 2
 plot(dd)
-write.csv(dd, file = './dataset/hinix_short3.csv')
-
+write.csv(dd, file = './hynix_short2.csv')
+sum(is.na(dd))#결측치 없어졌는지 확인
